8571277906297be98310d0bff1d3c979bc1991ee
Marshelby <elmarceloxd@gmail.com>
2025-05-22 16:42:13 -0400

    🖼️ GALERÍA profesional: diseño moderno con grilla de imágenes

--- src/sections/Galeria.jsx
+++ src/sections/Galeria.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import './Galeria.css';
+
+const imagenes = [
+  '/img/corte1.png',
+  '/img/corte2.png',
+  '/img/corte3.png',
+  '/img/corte4.png',
+  '/img/corte5.png',
+  '/img/corte6.png',
+];
 
 const Galeria = () => (
-  <section id="galeria" className="section">
-    <h2>Galería de Estilos</h2>
-    <p>Aquí pronto mostraremos algunos de nuestros mejores trabajos.</p>
+  <section id="galeria" className="galeria-section">
+    <h2 className="galeria-titulo">Galería de Estilos</h2>
+    <p className="galeria-subtitulo">Algunos de nuestros mejores trabajos realizados</p>
+    <div className="galeria-grid">
+      {imagenes.map((src, idx) => (
+        <div className="galeria-item" key={idx}>
+          <img src={src} alt={`Estilo ${idx + 1}`} />
+        </div>
+      ))}
+    </div>
   </section>
 );
 

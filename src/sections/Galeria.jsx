39e19d109189874edafaa0edc8133312ee5f9b85
Marshelby <elmarceloxd@gmail.com>
2025-09-03 18:43:47 -0400

    ✨ Actualización galería: reemplazo imágenes y añadidas corte7 a corte10

--- src/sections/Galeria.jsx
+++ src/sections/Galeria.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './Galeria.css';
 
 const imagenes = [
-  '/img/corte1.png',
-  '/img/corte2.png',
-  '/img/corte3.png',
-  '/img/corte4.png',
-  '/img/corte5.png',
-  '/img/corte6.png',
+  '/img/corte1.png', // se mantiene
+  '/img/corte2.png', // reemplazada
+  '/img/corte3.png', // reemplazada
+  '/img/corte4.png', // reemplazada
+  '/img/corte5.png', // reemplazada
+  '/img/corte6.png', // reemplazada
+  '/img/corte7.png', // nueva
+  '/img/corte8.png', // nueva
+  '/img/corte9.png', // nueva
+  '/img/corte10.png', // nueva
 ];
 
 const Galeria = () => (
@@ -17,7 +21,7 @@ const Galeria = () => (
     <div className="galeria-grid">
       {imagenes.map((src, idx) => (
         <div className="galeria-item" key={idx}>
-          <img src={src} alt={`Estilo ${idx + 1}`} />
+          <img src={src} alt={`Estilo ${idx + 1}`} loading="lazy" />
           <p className="galeria-texto">Estilo {idx + 1}</p>
         </div>
       ))}
